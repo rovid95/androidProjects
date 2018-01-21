6f5e4e9874c96ec01c313bf6181624dba05fdb46
ronald <rovid95@gmail.com>
2018-01-21 15:06:19 -0500

    App funcional: V2 qr code generator

---
+++ app/src/main/java/com/example/home/googlesignindemo/Residente.java
@@ -0,0 +1,82 @@
+package com.example.home.googlesignindemo;
+
+/**
+ * Created by ronal on 20-Jan-18.
+ */
+
+public class Residente {
+    private int cedula,passCodeRes;
+    private String solar,mz,nombre,apellido,contraseña,fileName,filePath;
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public int getPassCodeRes() {
+        return passCodeRes;
+    }
+
+    public void setPassCodeRes(int passCodeRes) {
+        this.passCodeRes = passCodeRes;
+    }
+
+    public String getSolar() {
+        return solar;
+    }
+
+    public void setSolar(String solar) {
+        this.solar = solar;
+    }
+
+    public String getMz() {
+        return mz;
+    }
+
+    public void setMz(String mz) {
+        this.mz = mz;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
